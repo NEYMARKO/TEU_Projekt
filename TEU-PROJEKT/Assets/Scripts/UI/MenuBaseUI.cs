bbb77bb03b663c8fcca147db8c7febe53e586b0c
NEYMARKO <marko.prosenjak5@gmail.com>
2025-01-01 13:36:03 +0100

    Checkpoint

---
+++ TEU-PROJEKT/Assets/Scripts/UI/MenuBaseUI.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MenuBaseUI : MonoBehaviour
+{
+    [SerializeField] PauseMenuDropdown menuDropdown;
+    [SerializeField] GameLoop gameLoop;
+    [Header("Buttons")]
+    [SerializeField] TextMeshProUGUI restartButtonTextObj;
+    [SerializeField] TextMeshProUGUI changeRegionButtonTextObj;
+    [SerializeField] TextMeshProUGUI quitButtonTextObj;
+    void Start()
+    {
+        SetupButtons();
+    }
+
+    private void OnEnable()
+    {
+        // Subscribe to the event
+        menuDropdown.OnLanguageChange += HandleLanguageChange;
+    }
+
+    private void OnDisable()
+    {
+        // Unsubscribe from the event to avoid memory leaks
+        menuDropdown.OnLanguageChange -= HandleLanguageChange;
+    }
+
+    private void HandleLanguageChange(object sender, Menu menu)
+    {
+        Debug.Log($"Language changed to: {menu.language}");
+        restartButtonTextObj.text = menu.shared.restart.ToUpper();
+        changeRegionButtonTextObj.text = menu.shared.changeRegion.ToUpper();
+        quitButtonTextObj.text = menu.shared.quit.ToUpper();
+    }
+    private void SetupButtons()
+    {
+        restartButtonTextObj.text = $"RESET";
+        changeRegionButtonTextObj.text = $"CHANGE LEVEL";
+        quitButtonTextObj.text = $"QUIT";
+    }
+
+    public void RestartGame()
+    {
+        gameLoop.RestartGame(gameObject);
+    }
+}
