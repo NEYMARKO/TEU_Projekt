5501a4f2634bbf4ee703f75929a18383521e1aed
NEYMARKO <marko.prosenjak5@gmail.com>
2025-01-13 11:53:17 +0100

    HighScore table updated based on active language

--- TEU-PROJEKT/Assets/Scripts/UI/HighScoresUI.cs
+++ TEU-PROJEKT/Assets/Scripts/UI/HighScoresUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Score
@@ -13,17 +14,41 @@ public class HighScoresUI : MonoBehaviour
     [SerializeField] DBManager dbManager;
     [SerializeField] Transform scoreParent;
     [SerializeField] GameObject scorePrefab;
+
+    [SerializeField] TextMeshProUGUI rankTextObj;
+    [SerializeField] TextMeshProUGUI scoreTextObj;
+    [SerializeField] TextMeshProUGUI timeTextObj;
+
+    private GameObject languageController;
+    private JSONMenuLoader menuContentLoader;
     private List<Score> topScores;
+    private string currentLanguage = "";
+    private HighScoreTable currentHighScoreTableContent;
     //private int lastRegionID;
     // Start is called before the first frame update
     private void Awake()
     {
         //lastRegionID = -1;
         topScores = new List<Score>();
+        languageController = GameObject.Find("LanguageController");
+        if (languageController == null)
+        {
+            Debug.LogError("LanguageController not found in the scene!");
+        }
+        else
+        {
+            menuContentLoader = languageController.GetComponent<JSONMenuLoader>();
+        }
     }
     private void OnEnable()
     {
         dbManager.GetTopScores(topScores);
+        if (currentLanguage != menuContentLoader.currentLanguage)
+        {
+            currentLanguage = menuContentLoader.currentLanguage;
+            currentHighScoreTableContent = menuContentLoader.GetHighScoreTableContent(currentLanguage);
+            HandleLanguageChange();
+        }
         RenderScores();
     }
 
@@ -48,5 +73,12 @@ public class HighScoresUI : MonoBehaviour
 
             tempObj.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
         }
+    }
+
+    private void HandleLanguageChange()
+    {
+        rankTextObj.text = currentHighScoreTableContent.rank.ToUpper();
+        scoreTextObj.text = currentHighScoreTableContent.score.ToUpper();
+        timeTextObj.text = currentHighScoreTableContent.time.ToUpper();
     }
 }
